391a7e45d356ae39f8403d691495ceef4526909b
dpeng <PDT1991@users.noreply.github.com>
2019-08-12 21:37:18 -0700

    Update SocketServer.java

--- MyApplication3/app/src/main/java/com/example/android/myapplication/SocketServer.java
+++ MyApplication3/app/src/main/java/com/example/android/myapplication/SocketServer.java
@@ -17,8 +17,8 @@ public class SocketServer {
 	public static void main(String[] args)  {
 		new SocketServer();
 	}
-    
-    public SocketServer() {
+	
+	public SocketServer() {
     	try {
     		server = new ServerSocket(port);
     		threadPool = Executors.newCachedThreadPool();
